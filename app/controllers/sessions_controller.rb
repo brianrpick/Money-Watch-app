e21c74608e0fd19c414482e821294a12ecf63f7a
Brian Pick <brianr.pick@gmail.com>
2016-12-12 18:42:28 -0800

    wrote sessions controller to include session ids and authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,28 @@
 class SessionsController < ApplicationController
-end
+  def new
+  end
 
+  def create
+    #find user by the id passed by email params
+    user = User.find_by(email: params[:email])
+    #if user is sucesfully authenticated save id in cookie redirect to '/' route
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:sucess] = "Logged in Sucessfully"
+      redirect_to "/"
+    else
+      #if user does not pass authentication, send flashing waring, reload login page
+      flash[:warning] = "Invalid email or password!"
+      redirect_to "/login"
+    end
+  end
+
+  def destroy
+  #end user session
+    
+    session[:user_id] = nil
+    flash[:sucess] = "You have sucesfully logged out"
+    redirect_to "/"
+  end
+end
 
