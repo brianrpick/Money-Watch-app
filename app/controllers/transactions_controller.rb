9017eddd6edc264fb843f0cad623a8216902b499
Brian Pick <brianr.pick@gmail.com>
2016-12-17 13:09:55 -0800

    Split positive/negative transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,9 +1,19 @@
 class TransactionsController < ApplicationController
   before_action :authenticate_user!
   def index
+    @positive = []
+    @negative = []
     @user_t = Plaid::User.create(:connect, 'wells', 'plaid_test', 'plaid_good')
+    @user_t.initial_transactions.each do |trans|
+      if trans.amount > 0
+        @positive << trans
+      else 
+        @negative << trans
+      end
+    end
+
   end
   def show
-    
+
   end 
 end
