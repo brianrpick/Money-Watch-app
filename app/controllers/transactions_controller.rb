09d8d35ce4562e004e408999349d83b8707f2f1c
Brian Pick <brianr.pick@gmail.com>
2016-12-17 12:54:00 -0800

    Added index page for trans, added transaction routes

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,9 @@
 class TransactionsController < ApplicationController
   before_action :authenticate_user!
-  def home
-    
+  def index
+    @user_t = Plaid::User.create(:connect, 'wells', 'plaid_test', 'plaid_good')
   end
+  def show
+    
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # creates restful routs for you (CRUD)
-  resources :users, :expenses
+  resources :users, :expenses, :transactions
   get '/', to: 'users#home'
 
   get "/login", to: "sessions#new"
