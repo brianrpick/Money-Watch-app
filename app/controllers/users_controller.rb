2701244caae49d4b3737abebc5bb3829967131c6
Brian Pick <brianr.pick@gmail.com>
2017-01-03 20:19:19 -0800

    Added User settings page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  def home
+  def settings
     @user = current_user
   end
 
@@ -7,14 +7,14 @@ class UsersController < ApplicationController
     render layout: false
   end
 
+
   def create
     @user = User.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       password: params[:password],
-      password_confirmation: params[:password_confirmation]
-    )
+      password_confirmation: params[:password_confirmation])
     if @user.save
       session[:user_id] = @user.id
       flash[:success] = 'Successfully created account!'
@@ -23,6 +23,21 @@ class UsersController < ApplicationController
       flash[:warning] = @user.errors.full_messages.join("<br>").html_safe
       render :new
     end
-
+  end
+  def update
+    @user = User.find(params[:id])
+    @user.assign_attributes(
+      first_name: params[:first_name], 
+      last_name: params[:last_name], 
+      savings_goal: params[:savings_goal], 
+      expense_budget: params[:expense_budget]
+      )
+    if @user.save
+      flash[:success] = 'User Info Updated'
+      redirect_to '/transactions'
+    else
+      flash[:warning] = @user.errors.full_messages.join("<br>").html_safe
+      render :settings
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # creates restful routs for you (CRUD)
   resources :users, :expenses, :transactions
+  get '/settings', to: "users#settings"
+
   get '/', to: 'pages#landing'
 
   get "/login", to: "sessions#new"
@@ -9,4 +11,6 @@ Rails.application.routes.draw do
   get "/logout", to: "sessions#destroy"
 
   get "/expenses/:id/calendar", to: "expenses#calendar"
+
+
 end
